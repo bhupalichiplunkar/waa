34560eebd218b6ee0975e070367cf4f4b1062343
bhupalichiplunkar <bhupali.chiplunkar@gmail.com>
2019-10-07 15:12:33 +0530

    Code file created.

---
+++ qqqqww/code.py
@@ -0,0 +1,18 @@
+# --------------
+z=x+int(y)
+
+
+# --------------
+output=prefix+str(z);
+
+print("DAsdasdasdasdasdasdas")
+print("DAsdasdasdasdasdasdas")
+print("DAsdasdasdasdasdasdas")
+print("DAsdasdasdasdasdasdas")
+
+print("DAsdasdasdasdasdasdas")
+print("DAsdasdasdasdasdasdas")
+print("DAsdasdasdasdasdasdas")
+print("DAsdasdasdasdasdasdas")
+
+
